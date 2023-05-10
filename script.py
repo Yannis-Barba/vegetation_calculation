a3edc24ba1f4885651702a620ea534395cd8a4e0
Yannis-Barba <yannis.barba.dev@gmail.com>
2023-05-10 17:28:09 +0200

    integrate dissolve into script : should help

--- script.py
+++ script.py
@@ -6,25 +6,20 @@ import pandas as pd
 print("### LOAD DATA ###")
 
 veget = gpd.read_file("./input_data/veget_strat.gpkg")
-network_buffer_dissolved = gpd.read_file("./input_data/network_metrop_buffer.gpkg")
-veget = veget.to_crs(3946)
-network_buffer_dissolved = network_buffer_dissolved.to_crs(3946)
-
+#network_buffer_dissolved = gpd.read_file("./input_data/network_metrop_buffer.gpkg")
 network_buffer_path = "./output_data/metrop_buffer_network.gpkg"
+network_edges_dissolved_path = "./output_data/metrop_buffer_dissolved.gpkg"
 network_path = "./input_data/metrop_walk_simplified.gpkg"
 
 ### TEST BBOX
-#veget = gpd.read_file("./input_data/bbox_veget.gpkg")
-#network_buffer_dissolved = gpd.read_file("./input_data/bbox_network_buffer.gpkg")
-#network_path = "./input_data/bbox_network.gpkg"
-#network_buffer_path = "./output_data/bbox_buffer_network_4171.gpkg"
+# veget = gpd.read_file("./input_data/bbox_veget.gpkg")
+# network_buffer_dissolved = gpd.read_file("./input_data/bbox_network_buffer.gpkg")
+# network_path = "./input_data/bbox_network.gpkg"
+# network_buffer_path = "./output_data/bbox_buffer_network_4171.gpkg"
+# network_edges_dissolved_path = "./output_data/bbox_metrop_buffer_dissolved.gpkg"
 
-print("### CLIP VEGET ###")
-
-clipped_veget = gpd.clip(veget, network_buffer_dissolved)
-
-clipped_veget_path = "./output_data/clipped_veget_3946.gpkg"
-clipped_veget.to_file(clipped_veget_path, driver="GPKG")
+veget = veget.to_crs(3946)
+# network_buffer_dissolved = network_buffer_dissolved.to_crs(3946)
 
 print("### BUFFERING NETWORK ###")
 
@@ -37,6 +32,21 @@ network_edges_buffer = gpd.GeoDataFrame(network_edges.drop("geometry", axis=1), 
 network_edges_buffer.crs = network.crs
 
 network_edges_buffer.to_file(network_buffer_path, layer="edges", driver="GPKG")
+
+print("### DISSOLVING BUFFERED NETWORK ###")
+
+network_edges_dissolved = network_edges_buffer.dissolve()
+
+network_edges_dissolved.to_file(network_edges_dissolved_path, layer="edges", driver="GPKG")
+
+print("### CLIP VEGET ###")
+
+clipped_veget = gpd.clip(veget, network_edges_dissolved)
+
+clipped_veget_path = "./output_data/clipped_veget_3946.gpkg"
+clipped_veget.to_file(clipped_veget_path, driver="GPKG")
+
+
 
 print("### JOIN VEGET NETWORK ###")
 
